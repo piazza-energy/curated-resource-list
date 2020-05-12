926f4ed9e88a671366c22086e6aed12bec742af6
Thomas Alisi <thomasalisi@gmail.com>
2020-05-12 15:21:17 +0100

    publish

---
+++ docs/_next/static/webpack/static/development/pages/index.js.de20637648519e278af6.hot-update.js
@@ -0,0 +1,48 @@
+webpackHotUpdate("static/development/pages/index.js",{
+
+/***/ "./components/category-link.js":
+/*!*************************************!*\
+  !*** ./components/category-link.js ***!
+  \*************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _next_link__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./next-link */ "./components/next-link.js");
+var _this = undefined,
+    _jsxFileName = "/Users/tom/Projects/piazza.energy/curated-resource-list/components/category-link.js";
+
+
+var __jsx = react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement;
+
+
+var CategoryLink = function CategoryLink(_ref) {
+  var name = _ref.name;
+  return __jsx(_next_link__WEBPACK_IMPORTED_MODULE_1__["default"], {
+    href: "/category/[name]",
+    as: "/category/".concat(name),
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 4,
+      columnNumber: 3
+    }
+  }, __jsx("a", {
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 5,
+      columnNumber: 5
+    }
+  }, name));
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (CategoryLink);
+
+/***/ })
+
+})
+//# sourceMappingURL=index.js.de20637648519e278af6.hot-update.js.map
